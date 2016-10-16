633fbc51269f81f70cb03a4463d91a3357b8978a
Guy Viner <gregorious@gmail.com>
2016-10-15 22:20:37 -0700

    pmTool in rails, starting out

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/' => 'home#index', as: :index
+  get '/about' => 'home#about', as: :about
+
+
+
+
+
+
+
+end
